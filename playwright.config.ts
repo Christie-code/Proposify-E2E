54f38496bc38101f7644ab53756dcbdb3fe1a826
Christie-code <62036667+Christie-code@users.noreply.github.com>
2024-03-10 19:46:13 -0300

    Cleanup

--- playwright.config.ts
+++ playwright.config.ts
@@ -44,14 +44,14 @@ export default defineConfig({
       use: { ...devices['Desktop Safari'] },
     },
     /* Test against branded browsers. */
-    {
-      name: 'Microsoft Edge',
-      use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    },
-    {
-      name: 'Google Chrome',
-      use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    },
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    // },
   ],
 
   /* Run your local dev server before starting the tests */
